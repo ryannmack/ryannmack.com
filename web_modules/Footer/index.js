3cc0dab846a7c48b45d7a3fbf123a264c792e2ca
Ryann Mack <rpierce@ryannpierce.com>
2016-08-11 13:09:42 -0500

    feat: add footer and GitHub image

--- web_modules/Footer/index.js
+++ web_modules/Footer/index.js
@@ -1,25 +1,32 @@
 import React, { Component } from "react"
 import { Link } from "react-router"
 
+import githubImage from "../images/github_logo.png"
+import twitterImage from "../images/twitter_logo.png"
 import styles from "./index.css"
 
 export default class Footer extends Component {
 
   render() {
     return (
-      <footer className={ styles.footer }>
-        <a
-          href={ process.env.PHENOMIC_HOMEPAGE }
-          className={ styles.link }
-        >
-          { "Powered by " }
-          <span className={ styles.reference }>
-            {  `<${ process.env.PHENOMIC_NAME} />` }
-          </span>
-        </a>
-
-        { " | " }
-        { "Pages: " }
+      <footer className="container">
+        <p className="footerInfo">
+          <span className="footerInfoMargin">hi@ryannmack.com</span>
+          <a href="https://twitter.com/ryann_mack" target="_blank">
+            <img
+              alt="Twitter Logo"
+              className="footerInfoMargin socialIcon"
+              src={ twitterImage }
+            />
+          </a>
+          <a href="https://github.com/ryannmack" target="_blank">
+            <img
+              alt="GitHub Logo"
+              className="socialIcon"
+              src={ githubImage }
+            />
+          </a>
+        </p>
         <Link
           className={ styles.link }
           to="/404.html"
@@ -29,7 +36,7 @@ export default class Footer extends Component {
         { ", " }
         <Link
           className={ styles.link }
-          to="/loading/"
+          to="/loading"
         >
           { "Loading" }
         </Link>
