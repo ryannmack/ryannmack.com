e0dbaae9ed5d391ec614b64ca93f0f958f76e0d1
Ryann Mack <rpierce@ryannpierce.com>
2016-08-11 12:50:30 -0500

    feat: add navigation links and styles

--- web_modules/Header/index.js
+++ web_modules/Header/index.js
@@ -1,55 +1,60 @@
 import React, { Component, PropTypes } from "react"
 import { Link } from "react-router"
 
-import styles from "./index.css"
-import Svg from "react-svg-inline"
-import twitterSvg from "../icons/iconmonstr-twitter-1.svg"
-import gitHubSvg from "../icons/iconmonstr-github-1.svg"
-
 export default class Header extends Component {
-  
+
   static contextTypes = {
     metadata: PropTypes.object.isRequired,
   };
 
   render() {
-    const {
-      pkg,
-    } = this.context.metadata
 
     return (
-      <header className={ styles.header }>
-        <nav className={ styles.nav }>
-          <div className={ styles.navPart1 }>
-            <Link
-              className={ styles.link }
-              to="/"
-            >
-              { "Home" }
-            </Link>
-          </div>
-          <div className={ styles.navPart2 }>
-            { pkg.twitter &&
-              <a
-                href={ `https://twitter.com/${pkg.twitter}` }
-                className={ styles.link }
+      <div className="navHeader clearfix">
+        <nav>
+          <ul className="nav nav-pills pull-xs-right">
+            <li className="nav-item">
+              <Link activeClassName="active" className="nav-link" to="/design">
+                Design
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClassName="active"
+                className="nav-link"
+                to="/development"
               >
-                <Svg svg={ twitterSvg } />
-                  { "Twitter" }
-              </a>
-            }
-            { pkg.repository &&
-              <a
-                href={ pkg.repository }
-                className={ styles.link }
+                Development
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClassName="active"
+                className="nav-link"
+                to="/project-management"
               >
-                <Svg svg={ gitHubSvg } />
-                { "GitHub" }
+                Project Management
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="mailto:ryann@ryannmack.com"
+              >
+                Contact
               </a>
-            }
-          </div>
+            </li>
+          </ul>
         </nav>
-      </header>
+        <Link className="navbar-brand" to="/">
+          <img
+            alt="Ryann Mack Logo"
+            className="navLogo"
+            src="images/rm_logo.png"
+          />
+        </Link>
+      </div>
+
     )
   }
 }
