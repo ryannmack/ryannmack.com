a9079577c76cdbfe15f4a503e47f8f9aa39f679a
Ryann Mack <rpierce@ryannpierce.com>
2016-08-19 15:54:10 -0400

    feat: add footer content and social icons

--- web_modules/Footer/index.js
+++ web_modules/Footer/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react"
 import { Link } from "react-router"
 
-import githubImage from "../images/github_logo.png"
-import twitterImage from "../images/twitter_logo.png"
+import Svg from "react-svg-inline"
+
+import githubSvg from "../images/github_logo.svg"
+import twitterSvg from "../images/twitter_logo.svg"
 import styles from "./index.css"
 
 export default class Footer extends Component {
@@ -10,41 +12,53 @@ export default class Footer extends Component {
   render() {
     return (
       <footer className="container">
-        <p className="footerInfo">
+        <div className="footerInfo">
           <a
             className="footerInfoMargin footerEmail"
             href="mailto:hi@ryannmack.com"
           >
             hi@ryannmack.com
           </a>
-          <a href="https://twitter.com/ryann_mack" target="_blank">
-            <img
+          <a
+            href="https://twitter.com/ryann_mack"
+            target="_blank"
+          >
+            <Svg
               alt="Twitter Logo"
-              className="footerInfoMargin socialIcon"
-              src={ twitterImage }
+              className="footerInfoMargin"
+              height="24px"
+              svg={ twitterSvg }
+              width="24px"
             />
           </a>
-          <a href="https://github.com/ryannmack" target="_blank">
-            <img
+          <a
+            href="https://github.com/ryannmack"
+            target="_blank"
+          >
+            <Svg
               alt="GitHub Logo"
-              className="socialIcon"
-              src={ githubImage }
+              height="24px"
+              svg={ githubSvg }
+              width="24px"
             />
           </a>
-        </p>
-        <Link
-          className={ styles.link }
-          to="/404.html"
-        >
-          { "404" }
-        </Link>
-        { ", " }
-        <Link
-          className={ styles.link }
-          to="/loading"
-        >
-          { "Loading" }
-        </Link>
+          // REMOVE 404 and Loading for testing.
+          <p>
+            <Link
+              className={ styles.link }
+              to="/404.html"
+            >
+              { "404" }
+            </Link>
+            { ", " }
+            <Link
+              className={ styles.link }
+              to="/loading"
+            >
+              { "Loading" }
+            </Link>
+          </p>
+        </div>
       </footer>
     )
   }
